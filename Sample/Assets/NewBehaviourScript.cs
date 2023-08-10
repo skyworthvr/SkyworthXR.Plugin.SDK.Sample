5f44c2df026bf03564c7e8112d5b74b7a3ff1f14
xieminghui <kevin.xie@skyworth.com>
2023-08-10 09:21:08 +0800

    add HelloVR sample

---
+++ Sample/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToSceen1() 
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+    public void ToSceen0()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
